59e03968ae07516241c944795ca47cf0b0323e19
Matteo Rizzo <matteo.rizzo.work@gmail.com>
2019-08-07 15:40:48 +0200

    annotations properly generated (but not tested)

--- scripts/data_format_conversion/functions/annotations.py
+++ scripts/data_format_conversion/functions/annotations.py
@@ -3,68 +3,9 @@ import pandas as pd
 from scripts.data_format_conversion.functions.utils import to_file_name, to_id, get_image_size, split_characters
 
 
-def get_annotation_data(image_base_name: str,
-                        path_to_images: str,
-                        label_mapping: pd.DataFrame,
-                        class_mapping: pd.DataFrame) -> pd.DataFrame:
+def convert_data(label: [], class_mapping: pd.DataFrame, image_width: int, image_height: int):
     """
-    Calculates the annotation data related to the labels of the given image.
-    :param image_base_name: the base name of the image (without extension)
-    :param path_to_images: the path where the images relative to the dataset are stored
-    :param label_mapping: the image-labels mapping
-    :param class_mapping: the class string to class number mapping
-    :return:
-    """
-
-    # Create a dataframe to store the annotation
-    annotation = pd.DataFrame(columns=['image_id', 'class', 'x_c', 'y_c', 'bb_width', 'bb_height'])
-
-    # Get the width and height of the image
-    image_width, image_height = get_image_size(os.path.join(path_to_images, to_file_name(image_base_name)))
-
-    # Get all the labels of the image as a string
-    all_labels = label_mapping.loc[image_base_name, 'labels']
-
-    # Convert the string of labels to list
-    all_labels = split_characters(all_labels)
-
-    for label in all_labels:
-        # Get the label data in the dataset format
-        unicode, x_bl, y_bl, abs_bb_width, abs_bb_height = label.split()
-
-        # Cast each data to int
-        x_bl = int(x_bl)
-        y_bl = int(y_bl)
-        abs_bb_width = int(abs_bb_width)
-        abs_bb_height = int(abs_bb_height)
-
-        # Convert the class string id to the corresponding integer value
-        class_number = class_mapping[class_mapping.Unicode == unicode].index
-
-        # Calculate the normalized coordinates of the center of the bounding box
-        x_c = (x_bl + abs_bb_width / 2) / image_width
-        y_c = (y_bl + abs_bb_height / 2) / image_height
-
-        # Calculate the normalized dimensions of the bounding box
-        bb_width = abs_bb_width / image_width
-        bb_height = abs_bb_height / image_height
-
-        # Append the label to the annotation
-        annotation.append([
-            image_base_name,
-            class_number,
-            x_c,
-            y_c,
-            bb_width,
-            bb_height
-        ])
-
-    return annotation
-
-
-def generate_annotations(path_to_annotations, path_to_images, path_to_map, path_to_classes):
-    """
-    Generates an annotation file for each image in the dataset.
+    Converts the label data to the YOLO data format.
 
     The dataset is provided with labels in the format: <unicode> <x_bl> <y_bl> <abs_bb_width> <abs_bb_height>
 
@@ -95,6 +36,76 @@ def generate_annotations(path_to_annotations, path_to_images, path_to_map, path_
     - <absolute_x_c> = <x_bl> + <abs_bb_width>  / 2
     - <absolute_y_c> = <y_bl> + <abs_bb_height> / 2
 
+    :param label: the label for one character in the image
+    :param class_mapping: the class number to character mapping
+    :param image_width: the absolute the width of the image
+    :param image_height: the absolute height of the image
+    :return: a list with the converted data
+    """
+
+    # Get the label data in the dataset format
+    unicode, x_bl, y_bl, abs_bb_width, abs_bb_height = label.split()
+
+    # Cast each data to int
+    x_bl = int(x_bl)
+    y_bl = int(y_bl)
+    abs_bb_width = int(abs_bb_width)
+    abs_bb_height = int(abs_bb_height)
+
+    # Convert the class string id to the corresponding integer value
+    class_number = class_mapping[class_mapping.Unicode == unicode].index[0]
+
+    # Calculate the normalized coordinates of the center of the bounding box
+    x_c = (x_bl + abs_bb_width / 2) / image_width
+    y_c = (y_bl + abs_bb_height / 2) / image_height
+
+    # Calculate the normalized dimensions of the bounding box
+    bb_width = abs_bb_width / image_width
+    bb_height = abs_bb_height / image_height
+
+    return [class_number,
+            x_c,
+            y_c,
+            bb_width,
+            bb_height
+            ]
+
+
+def get_annotation_data(image_base_name: str,
+                        path_to_images: str,
+                        label_mapping: pd.DataFrame,
+                        class_mapping: pd.DataFrame) -> pd.DataFrame:
+    """
+    Calculates the annotation data related to the labels of the given image.
+    :param image_base_name: the base name of the image (without extension)
+    :param path_to_images: the path where the images relative to the dataset are stored
+    :param label_mapping: the image-labels mapping
+    :param class_mapping: the class string to class number mapping
+    :return:
+    """
+
+    # Get all the labels of the image as a string
+    all_labels = label_mapping.loc[image_base_name, 'labels']
+
+    # Convert the string of labels to list
+    all_labels = split_characters(all_labels)
+
+    # Get the width and height of the image
+    img_width, img_height = get_image_size(os.path.join(path_to_images, to_file_name(image_base_name)))
+
+    # Create a list of lists to store the annotation data
+    annotation_data = [convert_data(label, class_mapping, img_width, img_height) for label in all_labels]
+
+    # Create a dataframe to store the whole annotation
+    annotation = pd.DataFrame(annotation_data, columns=['class', 'x_c', 'y_c', 'bb_width', 'bb_height'])
+
+    return annotation
+
+
+def generate_annotations(path_to_annotations, path_to_images, path_to_map, path_to_classes):
+    """
+    Generates an annotation file for each image in the dataset.
+
     :param path_to_map: the path to the image-labels mapping
     :param path_to_annotations: the path where di annotations of each image must be stored
     :param path_to_images: the path where the images relative to the dataset are stored
@@ -102,6 +113,13 @@ def generate_annotations(path_to_annotations, path_to_images, path_to_map, path_
     """
 
     print('Images are stored at {}.'.format(path_to_images))
+
+    file_list = [f for f in os.listdir(path_to_annotations)]
+    if file_list:
+        print('\nDeleting previously generated annotations at {}'.format(path_to_annotations))
+        for f in file_list:
+            os.remove(os.path.join(path_to_annotations, f))
+
     print('\nGenerating the annotations at {}...\n'.format(path_to_annotations))
 
     # If no annotations folder exists, it must be created
@@ -125,7 +143,7 @@ def generate_annotations(path_to_annotations, path_to_images, path_to_map, path_
                                          path_to_images=path_to_images,
                                          label_mapping=image_labels_map,
                                          class_mapping=class_numbers)
-
+        print(annotation.head())
         # Create the txt annotation file for the image
         annotation.to_csv(os.path.join(path_to_annotations,
                                        to_file_name(to_id(image_name), 'txt')),
